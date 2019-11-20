c87b0671bd1ba9051274737ca48ae3025b719f99
Nathalia Meneses Piedrahita <Nathalia_Meneses@satrack.com>
2019-11-20 16:06:24 -0500

    Adición de angular material a módulo core

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClientsComponent } from './clients/clients.component';
+import { AngularMaterialModule } from '../modules/angularmaterial.module';
 
 @NgModule({
   declarations: [ClientsComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    AngularMaterialModule,        
   ]
 })
 export class CoreModule { }
